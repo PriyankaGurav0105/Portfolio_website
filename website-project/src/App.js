d5e94c95f3a890f9c6d561d20644487c3eabd5d4
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2024-12-10 17:40:04 +0530

    hero section completed

--- website-project/src/App.js
+++ website-project/src/App.js
@@ -1,3 +1,4 @@
+import Home from "./components/Home.jsx";
 import NavBar from "./components/NavBar.jsx";
 
 
@@ -5,6 +6,7 @@ function App() {
   return (
   <div>
    <NavBar/>
+   <Home/>
   </div>
   );
 }
