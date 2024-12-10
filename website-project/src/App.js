7dd3af1579ac593b678dd3ef9a77e4c03ee163d4
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2024-12-10 19:00:20 +0530

    Basic portfolio is complete

--- website-project/src/App.js
+++ website-project/src/App.js
@@ -1,13 +1,23 @@
 import Home from "./components/Home.jsx";
 import NavBar from "./components/NavBar.jsx";
 import SocialLinks from "./components/SocialLinks.jsx";
+import About from "./components/About.jsx";
+import Projects from "./components/Projects.jsx";
+import Skills from "./components/Skills.jsx";
+import Contact from "./components/Contact.jsx";
 
-
+// refere this website to make changes in this portfolio https://priyankaguravmyportfoliowebsite.framer.website/
+// follow the list of changes from the notebook
+// Deloy the website on internet
 function App() {
   return (
   <div>
    <NavBar/>
    <Home/>
+   <About/>
+   <Projects/>
+   <Skills/>
+   <Contact/>
 
    <SocialLinks/>
   </div>
@@ -15,3 +25,5 @@ function App() {
 }
 
 export default App;
+
+
